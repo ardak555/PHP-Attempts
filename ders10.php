e32fb5100eea3f3da3b17c3209751a43b2d6e941
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-03-23 19:54:38 +0300

    Add files via upload

---
+++ ders10.php
@@ -0,0 +1,56 @@
+<?php
+for($i=0;$i<10;$i++)
+    echo $i."<br/>";
+for($i=10;$i>0;$i--)
+    echo  '<font size="'.$i.'">PHP</font><br/>';//font size 7 den başladığı için 4 tane aynı büyüklükte PHP yazar.
+
+$kitaplar=array("Php","Web Tasarım Klavuzu","Dreamweaver","Oracle","SQl Server");
+echo "<br/><br/>";
+for ($i=count($kitaplar);$i>0;$i--)
+    echo $kitaplar[$i-1]."<br/>";
+echo "<br/><br/>";
+
+for($i=0;$i<500;$i++)
+    while(($i%2==0)&&($i<20))
+    {
+        echo  $i."<br/>";
+        $i++;
+    }
+echo "<br/><br/>";
+
+$filmler=array("Matrix","Gladiator","Bourne Trilogy","Wanted","Pirates of Caribbean","X Men","Seven");
+foreach($filmler as $filmadi)
+    echo $filmadi."<br/>";
+echo "<br/><br/>";
+
+$filmler2=array("Keanu Reeves" => "Matrix","Russel Crowe" => "Gladiator","Matt Demon"=>"Bourne Trilogy","Angelina Jolie" => "Wanted","Jonny Deep" => "Pirates of Caribbean","Hugh Jackman" => "X Men","Brad Pitt" => "Seven");
+foreach($filmler2 as $aktor => $filmadi)
+    echo $aktor." -> ".$filmadi."<br/>";
+echo "<br/><br/>";
+
+echo $filmler2[$aktor];
+echo "<br/><br/>";
+
+//dongüden çıkma
+foreach($filmler2 as $aktor=>$filmadi )
+    if($aktor=="Jonny Deep")
+        break;
+    else
+        echo $aktor.",<br/>";
+    echo "<br/><br/>";
+for($i=1;$i<10;$i++)
+{
+    for($j=1;$j<10;$j++)
+    {
+        if($i*$j==18)
+            break 2;//?//
+        else
+            echo $i*$j."<br/>";
+    }
+}
+echo "<br/><br/>";
+
+$cumle="PHP ilk olarak Rasmus Lerdorf tarafından programlanmıştır";
+$dizi=explode(" ",$cumle);//fonksiyonunu bir değişken içindeki verileri belirlenen belirtece göre ayırmayı sağlayan fonksiyondur.
+foreach ($dizi as $index=>$parcalar)
+    echo $index."=>".$parcalar."<br/>";
